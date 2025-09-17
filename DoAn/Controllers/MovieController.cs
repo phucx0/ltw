2604572b5f9a22c3cf976118124934588892dfea
Kevin <hphucxyz981@gmail.com>
2025-09-17 22:45:03 +0700

    Enhance movie listing functionality and styling
    Updated MovieController to manage movie lists and created MoviesPageViewModel for data binding. Modified Movies.cshtml to utilize the new view model for dynamic display of trending and now showing movies. Improved CSS for better carousel layout and responsiveness. Adjusted JavaScript to dynamically size movie thumbnails based on carousel width.

--- DoAn/Controllers/MovieController.cs
+++ DoAn/Controllers/MovieController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DoAn.Models.Movies;
+using DoAn.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DoAn.Controllers
 {
@@ -6,7 +8,112 @@ namespace DoAn.Controllers
     {
         public IActionResult Movies()
         {
-            return View();
+            List<Movie> treding_movies = new List<Movie>
+            {
+                new Movie
+                {
+                    Title = "Thanh gươm diệt quỷ",
+                    Directors = new List<Director>
+                    {
+                        new Director { DirectorName = "Koyoharu Gotouge" },
+                        new Director { DirectorName = "Haruo Sotozaki" }
+                    },
+                    PosterUrl= "https://iguov8nhvyobj.vcdn.cloud/media/catalog/product/cache/1/thumbnail/240x388/c88460ec71d04fa96e628a21494d2fd3/p/o/poster_dm.jpg",
+                },
+                new Movie
+                {
+                    Title = "One Piece Film: Red",
+                    Directors = new List<Director>
+                    {
+                        new Director { DirectorName = "Eiichiro Oda" }
+                    },
+                    PosterUrl= "https://static.nutscdn.com/vimg/300-0/3032221de3bc86bcfbc979635b53d047.jpg",
+                },
+                new Movie
+                {
+                    Title = "Jujutsu Kaisen 0",
+                    Directors = new List<Director>
+                    {
+                        new Director { DirectorName = "Gege Akutami" }
+                    },
+                    PosterUrl= "https://static.nutscdn.com/vimg/300-0/09e4c57b152db1ecdb639eadef6b9356.jpg",
+                },
+                new Movie
+                {
+                    Title = "Your Name",
+                    Directors = new List<Director>
+                    {
+                        new Director { DirectorName = "Makoto Shinkai" }
+                    },
+                    PosterUrl= "https://static.nutscdn.com/vimg/300-0/1cb31180dc6e0d527a7faef6918b6bb3.jpg",
+                },
+                new Movie
+                {
+                    Title = "Spirited Away",
+                    Directors = new List<Director>
+                    {
+                        new Director { DirectorName = "Hayao Miyazaki" }
+                    },
+                    PosterUrl= "https://static.nutscdn.com/vimg/300-0/f416e981c5594516dcdedede5c359895.jpg",
+                }
+            };
+            List<Movie> now_showing_movies = new List<Movie>
+            {
+                new Movie
+                {
+                    Title = "Thanh gươm diệt quỷ",
+                    Directors = new List<Director>
+                    {
+                        new Director { DirectorName = "Koyoharu Gotouge" },
+                        new Director { DirectorName = "Haruo Sotozaki" }
+                    },
+                    PosterUrl= "https://iguov8nhvyobj.vcdn.cloud/media/catalog/product/cache/1/thumbnail/240x388/c88460ec71d04fa96e628a21494d2fd3/p/o/poster_dm.jpg",
+                },
+                new Movie
+                {
+                    Title = "One Piece Film: Red",
+                    Directors = new List<Director>
+                    {
+                        new Director { DirectorName = "Eiichiro Oda" }
+                    },
+                    PosterUrl= "https://static.nutscdn.com/vimg/300-0/3032221de3bc86bcfbc979635b53d047.jpg",
+                },
+                new Movie
+                {
+                    Title = "Jujutsu Kaisen 0",
+                    Directors = new List<Director>
+                    {
+                        new Director { DirectorName = "Gege Akutami" }
+                    },
+                    PosterUrl= "https://static.nutscdn.com/vimg/300-0/09e4c57b152db1ecdb639eadef6b9356.jpg",
+                },
+                new Movie
+                {
+                    Title = "Your Name",
+                    Directors = new List<Director>
+                    {
+                        new Director { DirectorName = "Makoto Shinkai" }
+                    },
+                    PosterUrl= "https://static.nutscdn.com/vimg/300-0/1cb31180dc6e0d527a7faef6918b6bb3.jpg",
+                },
+                new Movie
+                {
+                    Title = "Spirited Away",
+                    Directors = new List<Director>
+                    {
+                        new Director { DirectorName = "Hayao Miyazaki" }
+                    },
+                    PosterUrl= "https://static.nutscdn.com/vimg/300-0/f416e981c5594516dcdedede5c359895.jpg",
+                }
+            };
+            MoviesPageViewModel model = new MoviesPageViewModel
+            {
+                TredingMovies = treding_movies,
+                NowShowingMovies = now_showing_movies
+            };
+
+
+            return View(model);
         }
 
         public IActionResult Details()
