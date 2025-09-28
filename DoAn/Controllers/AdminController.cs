313932359bc7f1842fcd43ff925977b68c248883
Kevin <hphucxyz981@gmail.com>
2025-09-28 16:26:19 +0700

    Implement admin authentication and validation features
    Updated AdminController to handle user authentication, including password hashing and email validation. Introduced Validator class for Gmail format checking. Created Login view for admin login functionality and modified layout to support additional styles.

--- DoAn/Controllers/AdminController.cs
+++ DoAn/Controllers/AdminController.cs
@@ -1,9 +1,97 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DoAn.Models.Accounts;
+using DoAn.Models.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace DoAn.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly ModelContext _context;
+        private readonly PasswordHasher<User> _passwordHasher = new PasswordHasher<User>();
+
+        public AdminController (ModelContext context)
+        {
+            _context = context;
+        }
+
+        public string HashPassword(User user, string password)
+        {
+            return _passwordHasher.HashPassword(user, password);
+        }
+
+        public bool VerifyPassword(User user, string hashedPassword, string providedPassword)
+        {
+            var result = _passwordHasher.VerifyHashedPassword(user, hashedPassword, providedPassword);
+            return result == PasswordVerificationResult.Success;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            if (!Helpers.Validator.IsValidGmail(email))
+            {
+                ViewBag.Error = "Email không đúng định dạng";
+                return View();
+            }
+            //if (string.IsNullOrWhiteSpace(password) || password.Length < 8)
+            //{
+            //    ViewBag.Error = "Mật khẩu phải có ít nhất 8 ký tự";
+            //    return View();
+            //}
+
+            var user = _context.Users
+                .Include(u => u.Role)
+                .FirstOrDefault(u => u.Email == email && u.Role.RoleName == "admin");
+            //&& u.PasswordHash == HashPassword(password)
+            if (user == null) {
+                ViewBag.Error = "Tài khoản không tồn tại";
+                return View();
+            }
+            string hashed = HashPassword(user, password);
+            var result = VerifyPassword(user, user.PasswordHash, password);
+
+            if (result)
+            {
+                // Tạo Claims
+                var claims = new List<Claim> {
+                    new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
+                    new Claim(ClaimTypes.Name, user.FullName.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role.RoleName.ToString()),
+                    new Claim(ClaimTypes.Email, user.Email)
+                };
+
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var principal = new ClaimsPrincipal(identity);
+
+                // Đăng nhập và tạo cookie authentication
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    principal,
+                    new AuthenticationProperties
+                    {
+                        IsPersistent = true, 
+                        ExpiresUtc = DateTime.UtcNow.AddHours(12) 
+                    }
+                );
+                return RedirectToAction("Dashboard", "Admin");
+            }
+
+            ViewBag.Error = "Tài khoản hoặc mật khẩu không chính xác";
+            return View();
+        }
+
+        [Authorize(Roles = "admin, manager")]
         public IActionResult Dashboard()
         {
             return View();
---
+++ DoAn/Helpers/Validator.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DoAn.Helpers
+{
+    public class Validator
+    {
+        public static bool IsValidGmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            // Regex chỉ chấp nhận dạng ...@gmail.com
+            string pattern = @"^[a-zA-Z0-9._%+-]+@gmail\.com$";
+            return Regex.IsMatch(email, pattern);
+        }
+    }
+}
