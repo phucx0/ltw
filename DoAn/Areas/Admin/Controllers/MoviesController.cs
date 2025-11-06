55454b9db1339f342880cc2d8e430463818ac6d4
Kevin <hphucxyz981@gmail.com>
2025-11-06 23:10:13 +0700

    Refactor movie management functionality
    - Removed `MovieController.cs` and replaced it with `MoviesController.cs` for enhanced movie management features including create, edit, and details views.
    - Updated sidebar navigation to link to the new `Movies` controller.
    - Added `Create.cshtml`, `Details.cshtml`, and `Edit.cshtml` for movie CRUD operations.
    - Modified `Index.cshtml` to improve filtering, sorting, and action buttons.
    - Updated `Movie.cs` model to make `AgeRating` non-nullable.
    - Enhanced layout and styling in `layout.css` for better admin page presentation.

---
+++ DoAn/Areas/Admin/Controllers/MoviesController.cs
@@ -0,0 +1,54 @@
+﻿using DoAn.Models.Data;
+using DoAn.Models.Movies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoAn.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "admin,manager")]
+    public class MoviesController : Controller
+    {
+        private ModelContext _context;
+        public MoviesController (ModelContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var list = _context.Movies.Take(10).ToList();
+            return View(list);
+        }
+
+        public IActionResult Create() {
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var movie = await _context.Movies
+                .Include(m => m.AgeRating)
+                .Include(m => m.MovieActors)
+                    .ThenInclude(ma => ma.Actor)
+                .Include(m => m.MovieDirectors)
+                    .ThenInclude(md => md.Director)
+                .FirstOrDefaultAsync(m => m.MovieId == id);
+            return View(movie);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            return View(movie);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Movie movie)
+        {
+            return RedirectToAction("Index", "Movies");
+            //var movie = await _context.Movies.FindAsync(id);
+            //return View(movie);
+        }
+    }
+}
