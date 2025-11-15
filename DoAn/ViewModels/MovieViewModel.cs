24d0e484dbf14d1e45066e42c848d9b58f77de01
Le Nguyen Gia Quan <lenguyengiaquan306@gmail.com>
2025-11-15 20:33:23 +0700

    Add create movie feature

--- DoAn/ViewModels/MovieViewModel.cs
+++ DoAn/ViewModels/MovieViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using DoAn.Models.Movies;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.Collections.Generic;
 
 namespace DoAn.ViewModels
@@ -10,7 +11,7 @@ namespace DoAn.ViewModels
         public string Title { get; set; }
         public string Description { get; set; }
         public int Duration { get; set; }
-        public string Genre { get; set; }
+        public string? Genre { get; set; }
         public int? RatingId { get; set; }
         public DateTime ReleaseDate { get; set; }
         public string PosterUrl { get; set; }
@@ -19,16 +20,22 @@ namespace DoAn.ViewModels
         public string Status { get; set; }
         public decimal? ImdbRating { get; set; }
         public List<string> SelectedGenres { get; set; } = new();
+        [ValidateNever]
         public List<string> AllGenres { get; set; } = new() { "Action", "Comedy", "Crime", "Drama", "Fantasy", "Horror", "Sci-Fi" };
 
 
         // Actors & Directors
+        [ValidateNever]
         public List<int> SelectedActorIds { get; set; } = new();
+        [ValidateNever]
         public List<int> SelectedDirectorIds { get; set; } = new();
 
         // For dropdown list
+        [ValidateNever]
         public List<Actor> AllActors { get; set; }
+        [ValidateNever]
         public List<Director> AllDirectors { get; set; }
+        [ValidateNever]
         public List<AgeRating> AllRatings { get; set; }
     }
 }
